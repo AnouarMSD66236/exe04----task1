3df03de26fe4bfeffd0f1fa77106376873b17a52
AnouarMSD66236 <anouarmoussaid21@gmail.com>
2024-03-14 22:15:10 +0100

    task 1-----exe4

---
+++ src/RectangleDemo.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+
+  class RectangleArea {
+       double a,b,area;
+       void getData()  throws IOException {
+           BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+           System.out.println("this program will calculate the area of the rectangle");
+           System.out.println("Enter the length of the side a:");
+           a = Double.parseDouble(br.readLine());
+           System.out.println("Enter the length of the side b:");
+           b =  Double.parseDouble(br.readLine());
+
+       }
+       void computeField() { area = a * b;}
+       void fieldDisplay(){
+           System.out.println("the area of the rectangle with the side a = ");
+           System.out.printf("%2.2f",a);
+           System.out.println("and side b = ");
+           System.out.printf("%2.2f",b);
+           System.out.println("is =");
+           System.out.printf("%2.2f.\n", area);
+       }
+
+
+
+}
+public class RectangleDemo {
+    public static void main(String[] args) {
+        RectangleArea ar = new RectangleArea();
+        ar.getData();
+        ar.computeField();
+        ar.fieldDisplay();
+    }
+}
